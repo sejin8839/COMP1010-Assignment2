71d3000cbd162af0f2acc487171a7d75d6c92cee
sejin8839 <sejinon01@gmail.com>
2023-03-20 21:34:35 +0900

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -12,3 +12,18 @@ Source code File name: problem3.py
 
 
 '''
+
+
+
+n = int(input("Enter a positive integer: "))
+sum = 0
+for i in range(1, n+1):
+    sum += 1/i
+print("The sum of the series is:", round(sum, 2))
+
+
+n = int(input("Enter a positive integer: "))
+sum = 0
+for i in range(1, n+1):
+    sum += pow(n, n)/n
+print("The sum of the series is:", round(sum, 2))
